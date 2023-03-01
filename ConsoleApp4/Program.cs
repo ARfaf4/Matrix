55c2c1d7825aee5d78757040353092d860de7f0a
IliasAntn <antoniou.ilias19@gmail.com>
2023-03-01 15:27:13 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("From what rank is the matrix: ");
+            int rank = int.Parse(Console.ReadLine());
+            List<double> matrix = new List<double>();
+            for (int i = 0; i <= rank; i++)
+            {
+                var row = Console.ReadLine().Split(' ').ToList().Select(double.Parse);
+                matrix.Add(row);
+            }
+            var A = Matrix<double>.Build.DenseOfArray(new double[,]{
+                 { 0, 1, 1, 2},
+                 { 2 , 1, 2, -1},
+                 { 1, -1, 3, -2},
+                 { 3, 2, -1, 4}
+                  });
+            var b = Vector<double>.Build.Dense(new double[]
+                { -3, 6 , 10 , -15});
+            var x = A.Solve(b);
+
+            Console.WriteLine(x);
+        }
+    }
+}
