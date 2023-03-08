8daf2c7f7be4bf962676c1fd83f797ecc8362423
IliasAntn <antoniou.ilias19@gmail.com>
2023-03-08 15:09:12 +0200

    NOT HARD CODED MATRIX AND VECTOR + DETERMINANT

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -14,22 +14,61 @@ namespace ConsoleApp4
             Console.WriteLine("From what rank is the matrix: ");
             int rank = int.Parse(Console.ReadLine());
             List<double> matrix = new List<double>();
-            for (int i = 0; i <= rank; i++)
+            List<double> vector = new List<double>();
+            for (int i = 0; i < rank; i++)
             {
-                var row = Console.ReadLine().Split(' ').ToList().Select(double.Parse);
-                matrix.Add(row);
+                Console.WriteLine("Enter row " + i + " :");
+                var row = Console.ReadLine();
+                var list = row.Split(' ').ToList();
+                foreach (var item in list)
+                {
+                    double n = double.Parse(item);
+                    matrix.Add(n);
+                }              
             }
-            var A = Matrix<double>.Build.DenseOfArray(new double[,]{
-                 { 0, 1, 1, 2},
-                 { 2 , 1, 2, -1},
-                 { 1, -1, 3, -2},
-                 { 3, 2, -1, 4}
-                  });
-            var b = Vector<double>.Build.Dense(new double[]
-                { -3, 6 , 10 , -15});
-            var x = A.Solve(b);
+            Console.WriteLine("Enter vector:");
+            var row1 = Console.ReadLine();
+            var list1 = row1.Split(' ').ToList();
+            foreach (var item in list1)
+            {
+                double n = double.Parse(item);
+                vector.Add(n);
+            }
 
-            Console.WriteLine(x);
+            if (rank == 3)
+            {
+                var A = Matrix<double>.Build.DenseOfArray(new double[,]{
+                 { matrix[0], matrix[1], matrix[2]},
+                 { matrix[3] , matrix[4], matrix[5]},
+                 { matrix[6], matrix[7], matrix[8]},
+                  });
+                var b = Vector<double>.Build.Dense(new double[]
+                { vector[0] , vector[1] , vector[2] });
+                var x = A.Solve(b);
+
+                Console.WriteLine(x);
+                Console.ReadKey();
+            }
+            else if (rank == 4 )
+            {
+                var A = Matrix<double>.Build.DenseOfArray(new double[,]{
+                 { matrix[0], matrix[1], matrix[2], matrix[3]},
+                 { matrix[4] , matrix[5], matrix[6], matrix[7]},
+                 { matrix[8], matrix[9], matrix[10], matrix[11]},
+                  });
+                var b = Vector<double>.Build.Dense(new double[]
+                { vector[0], vector[1] , vector[2] , vector[3]});
+                var x = A.Solve(b);
+
+                Console.WriteLine(x);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Invalid matrix rank!!!");
+            }
+            
+            
         }
     }
 }
